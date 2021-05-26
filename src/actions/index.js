703ca4e54f2f13cd2af091f9a6be0329f4dcbaf4
abdelmonem88 <abdelmonem1888@gmail.com>
2021-05-26 06:07:18 +0200

    add all functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,23 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+ await dispatch(fetchPosts());
+ const userIds = _.uniq(_.map(getState().posts, "userId"));
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+//  const userIds = new Set([...getState().posts.map((post) => post.userId)]);
+
+ userIds.forEach((id) => dispatch(fetchUser(id)));
+};
+
+export const fetchPosts = () => async (dispatch) => {
+ const response = await jsonPlaceholder.get("/posts");
+
+ dispatch({ type: "FETCH_POSTS", payload: response.data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+ const response = await jsonPlaceholder.get(`/users/${id}`);
+
+ dispatch({ type: "FETCH_USER", payload: response.data });
 };
